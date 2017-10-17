0d92d1de8f70903e8bbc66fda22f12ca5c34e628
x <helenlanxh@outlook.com>
2017-10-17 23:29:41 +1100

    Create Data Imputation.R

---
+++ Data Imputation.R
@@ -0,0 +1,4 @@
+# Single Imputation, replace each missing value with the genotype that the most frequent in the sampe
+set.seed(1234)
+round(apply(is.na(breast_cancer),2,summ)/dim(breast_cancer)[1],3)
+data<-na.rougnfix(breast_cancer)
